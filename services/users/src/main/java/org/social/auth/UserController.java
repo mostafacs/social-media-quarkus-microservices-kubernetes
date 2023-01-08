3ada4125181570c38d06a11820a6b898b6045cc7
mostafa <engmostafa.cs@gmail.com>
2023-01-08 05:36:52 +0200

    rename auth service

--- services/users/src/main/java/org/social/auth/UserController.java
+++ services/users/src/main/java/org/social/auth/UserController.java
@@ -24,7 +24,7 @@ import java.util.Collections;
 @Path("/register")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
-public class RegisterController {
+public class UserController {
 
     @Inject
     Keycloak keycloak;
